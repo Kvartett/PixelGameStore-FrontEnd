6ceb6c1e52081a5ea111f3b149a05b91e65e1df0
Luiz <felipedevluiz@gmail.com>
2022-11-24 20:48:41 -0300

    style: add store name at logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,11 +3,24 @@ import styled from "styled-components"
 
 export default function Logo() {
     return (
-        <LogoSize src={logo} />
+        <LogoContainer>
+            <LogoSize src={logo} />
+            <h1>Pixel Game Store</h1>
+        </LogoContainer>
     )
 }
 
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    h1 {
+        font-size: 22px;
+        font-weight: 600;
+    }
+`
+
 const LogoSize = styled.img`
-    width: 25px;
-    height: 25px;
+    width: 28px;
+    height: 28px;
+    margin-right: 10px;
 `
